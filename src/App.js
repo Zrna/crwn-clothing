9c05b8905ee1f96d62ecf5f1bcdaa22bf3a9a582
Zrna <lukazrnic5@gmail.com>
2020-04-08 11:01:36 +0200

    Added Sign in and Sign up page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
+import SignInAndSignUpPage from './pages/SignInAndSignUpPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/SignInAndSignUpPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+import SignIn from '../../components/SignIn';
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
