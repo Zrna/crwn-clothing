83f2354b6aa1a41dd8209d02c252ff012995c494
Zrna <lukazrnic5@gmail.com>
2020-04-11 10:31:54 +0200

    Collection page content implementation, data normalization

--- src/pages/CollectionPage/index.jsx
+++ src/pages/CollectionPage/index.jsx
@@ -4,13 +4,20 @@ import { selectCollection } from '../../store/shop/shop.selectors';
 
 import './styles.scss';
 
+// eslint-disable-next-line no-unused-vars
 import CollectionItem from '../../components/CollectionItem';
 
 const CollectionPage = ({ collection }) => {
-  console.log('collection is:', collection);
+  const { title, items } = collection;
+
   return (
     <div className='collection-page'>
-      <h2>collection page</h2>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+      </div>
     </div>
   );
 };
--- src/store/shop/shop.selectors.js
+++ src/store/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,7 +7,8 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+// list of elements inside of object instead of array (objects inside one object) is called data normalization
 export const selectCollection = collectionUrlParam => createSelector(
   [selectCollections],
-  collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+  collections => collections[collectionUrlParam]
 );
