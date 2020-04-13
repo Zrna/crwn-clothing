a196edb583cedbfb50837585fb14d2ff5be34a16
Zrna <lukazrnic5@gmail.com>
2020-04-13 13:43:29 +0200

    Converting Collection page to styled components

--- src/pages/CollectionPage/index.jsx
+++ src/pages/CollectionPage/index.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../store/shop/shop.selectors';
 
-import './styles.scss';
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer
+} from './styles';
 
-// eslint-disable-next-line no-unused-vars
 import CollectionItem from '../../components/CollectionItem';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
 
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {
           items.map(item => <CollectionItem key={item.id} item={item} />)
         }
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
