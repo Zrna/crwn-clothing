0a58de06b077af3ce504c295e81ae402c0600bb8
Zrna <lukazrnic5@gmail.com>
2020-04-15 17:18:27 +0200

    Clear Cart Saga

--- src/store/shop/shop.sagas.js
+++ src/store/shop/shop.sagas.js
@@ -1,4 +1,9 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import {
+  takeLatest,
+  call,
+  put,
+  all
+} from 'redux-saga/effects';
 import { ShopActionTypes } from './shop.types';
 
 import {
@@ -27,4 +32,10 @@ export function* fetchCollectionsAsync() {
 
 export function* fetchCollectionsStart() {
   yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+}
+
+export function* shopSagas() {
+  yield all([
+    call(fetchCollectionsStart)
+  ]);
 }
