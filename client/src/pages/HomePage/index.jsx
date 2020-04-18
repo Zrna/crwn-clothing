6acf7e2e1b5ca62d2d46886d27a6fb2cf9cff247
Zrna <lukazrnic5@gmail.com>
2020-04-18 13:19:22 +0200

    Added Profiler for analyze rendering Directory component

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import Directory from '../../components/Directory';
 
@@ -7,7 +7,12 @@ import { HomePageContainer } from './styles';
 const HomePage = () => {
   return (
     <HomePageContainer>
-      <Directory />
+      <Profiler
+        id='Directory'
+        onRender={(id, phase, actualDuration) => console.log({ id, phase, actualDuration })}
+      >
+        <Directory />
+      </Profiler>
     </HomePageContainer>
   );
 };
