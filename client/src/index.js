70e76638b3a780ca0a5872d13dbc5f76e3a6c735
Zrna <lukazrnic5@gmail.com>
2020-04-18 14:37:25 +0200

    App updated to be PWA

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 import App from './App';
 
 import { store, persistor} from './store';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
